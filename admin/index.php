1c29a744ca0c490b52bacc7aae46708e2680a459
Kum Jude Tem <judekum14@gmail.com>
2021-03-29 03:26:47 +0100

    Admin panel created with styling

---
+++ admin/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Food Order | Home</title>
+
+  <!-- bootstrap -->
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+  <!-- menu -->
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Admin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Category</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1">Food</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1">Order</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- MAIN SECTION -->
+  <div class="main-content">
+    <div class="wrapper">
+      <h2>Dashboard</h2>
+
+      <div class="col-4 text-center">
+        <h2>6</h2>
+        Categories
+      </div>
+      <div class="col-4 text-center">
+        <h2>6</h2>
+        Categories
+      </div>
+      <div class="col-4 text-center">
+        <h2>6</h2>
+        Categories
+      </div>
+      <div class="col-4 text-center">
+        <h2>6</h2>
+        Categories
+      </div>
+
+      <div class="clearfix"></div>
+    </div>
+  </div>
+
+  <!-- FOOTER -->
+  <div class="footer">
+    <div class="wrapper">
+      <p class="text-center">Copyright 2021. All Rights Reserved by BurgerFood.</p>
+    </div>
+  </div>
+
+
+
+
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js">
+  </script>
+</body>
+
+</html>
